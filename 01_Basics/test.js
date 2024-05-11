d11cc61fb26e30a888f5f495bd3d71735f3378a1
hunterkid498 <152261302+hunterkid498@users.noreply.github.com>
2024-05-11 14:06:58 +0000

    datatypes

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,12 @@
-console.log("Sumit")
+const accountId = 144556
+let accountEmail = "abc@gmail.com"
+var accountpassword = "1234567"
+
+accountCity = "Pune"
+
+// accountId = 23
+
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountpassword, accountCity])
